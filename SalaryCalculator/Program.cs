ccbc5cadb2291d15cf3f23bae3c0429a1d4d736a
Truc Chau <tc9538@cs.ship.edu>
2019-03-05 22:32:07 -0500

    add source code

---
+++ SalaryCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your hourly rate: ");
+            double hourlyRate = double.Parse(Console.ReadLine());
+            Console.WriteLine("How many hours did you work this week?");
+            double workHours = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your gross income for this week is $" + hourlyRate*workHours);
+
+        }
+    }
+}
